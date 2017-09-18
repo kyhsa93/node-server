ca96a8b02646b54a60be8af6db8951fe0ea348c2
unknown <>
2017-09-18 19:30:20 +0900

    config port

---
+++ index.js
@@ -0,0 +1,15 @@
+'use strict'
+
+//서버 환경 설정
+var CONFIG = require('./src/config/config');
+var port = CONFIG.port;
+
+var express = require('express');
+var bparser = require('body-parser');
+
+var app = express();
+
+//server start
+app.listen(port, () => {
+    console.log(`qexpress app listening on port ${port}!`);
+});
