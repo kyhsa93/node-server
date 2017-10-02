cc4c600ae23451fcdea8f72f0864f780583af771
kyhsa93 <kyhsa93@naver.com>
2017-10-02 17:37:32 +0900

    Add GET TEST code

--- src/routes/get-test/get-test.js
+++ src/routes/get-test/get-test.js
@@ -1,3 +1,5 @@
 module.exports = (request, response) => {
-    
+    console.log('GET DATA : ', request.params);
+    var result = request.params;
+    result ? response.sendStatus(200) : response.sendStatus(400);
 };
